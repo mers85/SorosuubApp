c8bc756c0640102af862edb473b1ecc9a3c12289
Mayra <mers85@gmail.com>
2021-12-08 11:57:11 +0100

    Instalation of rails event store gem

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module SorosuubApp
   class Application < Rails::Application
+    config.paths.add "lib", eager_load: true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
